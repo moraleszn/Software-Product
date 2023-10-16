a47d6edee9307ec18b6b33396e4907a9cd18d1c5
diegomorales <83707362+moraleszn@users.noreply.github.com>
2023-10-15 21:16:52 -0300

    Update script.js

--- app/frontend/script.js
+++ app/frontend/script.js
@@ -5,8 +5,32 @@ function addTask() {
     if (taskText !== "") {
         const taskList = document.getElementById("taskList");
         const li = document.createElement("li");
+        li.className = "task";
         li.textContent = taskText;
+        li.onclick = () => toggleCompletion(li);
         taskList.appendChild(li);
         taskInput.value = "";
     }
+}
+
+function toggleCompletion(task) {
+    task.classList.toggle("completed");
+}
+
+function filterTasks(filter) {
+    const tasks = document.querySelectorAll("li.task");
+    tasks.forEach((task) => {
+        if (filter === "all" || task.classList.contains(filter)) {
+            task.style.display = "block";
+        } else {
+            task.style.display = "none";
+        }
+    });
+}
+
+function clearCompletedTasks() {
+    const completedTasks = document.querySelectorAll("li.task.completed");
+    completedTasks.forEach((task) => {
+        task.remove();
+    });
 }
