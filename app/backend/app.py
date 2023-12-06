62a9b8eb06f824b34a138dcc160317ce52edcf6c
diegomorales <83707362+moraleszn@users.noreply.github.com>
2023-12-06 15:59:05 -0300

    Add files via upload

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,29 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-db = SQLAlchemy(app)
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Boolean, default=False)
-
-@app.route('/tasks', methods=['GET', 'POST'])
-def tasks():
-    if request.method == 'GET':
-        tasks = Task.query.all()
-        task_list = [{'id': task.id, 'text': task.text, 'completed': task.completed} for task in tasks]
-        return jsonify(task_list)
-    elif request.method == 'POST':
-        data = request.get_json()
-        new_task = Task(text=data['text'])
-        db.session.add(new_task)
-        db.session.commit()
-        return jsonify({'message': 'Task created successfully'}), 201
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    with app.app_context():  # Cria um contexto de aplicação
-        db.create_all()  # Cria as tabelas do banco de dados
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
--- app/frontend/script.js
+++ app/frontend/script.js
@@ -1,36 +1,67 @@
+let tasks = [];
+
 function addTask() {
     const taskInput = document.getElementById("task");
     const taskText = taskInput.value.trim();
 
     if (taskText !== "") {
-        const taskList = document.getElementById("taskList");
-        const li = document.createElement("li");
-        li.className = "task";
-        li.textContent = taskText;
-        li.onclick = () => toggleCompletion(li);
-        taskList.appendChild(li);
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false,
+            timestamp: new Date(),
+            dueDate: null
+        };
+
+        tasks.push(newTask);
+        renderTasks();
         taskInput.value = "";
     }
 }
 
-function toggleCompletion(task) {
-    task.classList.toggle("completed");
+function toggleCompletion(taskId) {
+    const task = tasks.find(t => t.id === taskId);
+    if (task) {
+        task.completed = !task.completed;
+        renderTasks();
+    }
 }
 
-function filterTasks(filter) {
-    const tasks = document.querySelectorAll("li.task");
-    tasks.forEach((task) => {
-        if (filter === "all" || task.classList.contains(filter)) {
-            task.style.display = "block";
-        } else {
-            task.style.display = "none";
-        }
-    });
+function filterTasks(status) {
+    const filteredTasks = status === "all" ? tasks :
+        status === "pending" ? tasks.filter(task => !task.completed) :
+        tasks.filter(task => task.completed);
+
+    renderTasks(filteredTasks);
 }
 
 function clearCompletedTasks() {
-    const completedTasks = document.querySelectorAll("li.task.completed");
-    completedTasks.forEach((task) => {
-        task.remove();
+    tasks = tasks.filter(task => !task.completed);
+    renderTasks();
+}
+
+function renderTasks(filteredTasks = tasks) {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+
+    filteredTasks.forEach(task => {
+        const li = document.createElement("li");
+        li.className = `task${task.completed ? " completed" : ""}`;
+        li.innerHTML = `
+            <span>${task.text}</span>
+            <span class="timestamp">${formatTimestamp(task.timestamp)}</span>
+            <span class="due-date">${formatDueDate(task.dueDate)}</span>
+        `;
+        li.onclick = () => toggleCompletion(task.id);
+        taskList.appendChild(li);
     });
+}
+
+function formatTimestamp(timestamp) {
+    const options = { hour: "numeric", minute: "numeric" };
+    return new Intl.DateTimeFormat("en-US", options).format(timestamp);
+}
+
+function formatDueDate(dueDate) {
+    return dueDate ? `Due: ${dueDate.toLocaleDateString()}` : "";
 }
